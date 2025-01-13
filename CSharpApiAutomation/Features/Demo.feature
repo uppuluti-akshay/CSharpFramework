e1f9d51470861bf393cc5f2a92c0e6698aabf523
Uppuluti Akshay <uppuluti.akshay@gmail.com>
2025-01-13 08:08:25 +0530

    Added APi and UI framework with insame solution file

---
+++ CSharpApiAutomation/Features/Demo.feature
@@ -0,0 +1,11 @@
+﻿@api
+Feature: Demo
+
+A short summary of the feature
+
+@tc:1234
+Scenario: Create a new User
+	Given User with name "Akshay"
+	And User with job "Manager"
+	When send request to create user
+	Then validate the user is created
---
+++ CSharpApiAutomation/Models/ApiModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowApiFramework.Models
+{
+    public class ApiModels
+    {
+        public Object Header { get; set; }
+        public Object Items { get; set; }
+
+       
+
+    }
+
+    public class Header
+    {
+        public string NameOfUser { get; set; }
+        public string DescriptionOfUser { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Iteams
+    {
+        public decimal age { get; set; }
+        public string UserOfItems { get; set; }
+        public DateTime timeOfExecution { get; set; }
+        public string asOfDate { get; set; }
+        public string asOfTime { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
